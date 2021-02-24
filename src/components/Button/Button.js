247001411a82c7709d36d36e6df966e96c9c8a19
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-02-25 00:00:44 +0530

    change to button icon

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './Button.css';
-
+import React from "react";
+import "./Button.css";
 
 function Button({ icon, text, onClick }) {
-    return (
-            <div className="button-game" onClick={onClick}>
+  return (
+    <button onClick={onClick} className="button-game">
+      {icon ? <img className="button-icon" src={icon} alt="" /> : null}
+      <span>{text}</span>
+    </button>
+    /* <div className="button-game" onClick={onClick}>
                 {icon ? <img className="icon-reload" src={icon} alt="icon" /> : null}
                 <span>{text}</span>
-            </div>
-    );
+            </div> */
+  );
 }
 export default Button;
